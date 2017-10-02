f7fcd95b5afc7028cd9583a70dcea120a9078ee0
daiheekim <daiheekim@daihees-mbp.ottriv.can.ibm.com>
2017-10-02 12:14:28 -0400

    Thunk Checkpoint

--- jitbuilder/release/src/Thunk.cpp
+++ jitbuilder/release/src/Thunk.cpp
@@ -1,23 +1,19 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2016 IBM Corp. and others
  *
- * This program and the accompanying materials are made available under
- * the terms of the Eclipse Public License 2.0 which accompanies this
- * distribution and is available at https://www.eclipse.org/legal/epl-2.0/
- * or the Apache License, Version 2.0 which accompanies this distribution and
- * is available at https://www.apache.org/licenses/LICENSE-2.0.
+ * (c) Copyright IBM Corp. 2016, 2016
  *
- * This Source Code may also be made available under the following
- * Secondary Licenses when the conditions for such availability set
- * forth in the Eclipse Public License, v. 2.0 are satisfied: GNU
- * General Public License, version 2 with the GNU Classpath
- * Exception [1] and GNU General Public License, version 2 with the
- * OpenJDK Assembly Exception [2].
+ *  This program and the accompanying materials are made available
+ *  under the terms of the Eclipse Public License v1.0 and
+ *  Apache License v2.0 which accompanies this distribution.
  *
- * [1] https://www.gnu.org/software/classpath/license.html
- * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *      The Eclipse Public License is available at
+ *      http://www.eclipse.org/legal/epl-v10.html
  *
- * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ *      The Apache License v2.0 is available at
+ *      http://www.opensource.org/licenses/apache2.0.php
+ *
+ * Contributors:
+ *    Multiple authors (IBM Corp.) - initial implementation and documentation
  *******************************************************************************/
  
 #include <iostream>
@@ -28,6 +24,17 @@
 #include "ilgen/TypeDictionary.hpp"
 #include "ilgen/ThunkBuilder.hpp"
 
+static uintptr_t floatHelper(float f){
+	union F
+	{
+		uintptr_t uip;
+		float data;
+	};
+	F fUnion;
+	fUnion.uip = 0;
+	fUnion.data = f;
+	return fUnion.uip;
+}
 
 static void
 thunk_1b(int8_t b)
@@ -41,6 +48,24 @@ thunk_1b_2(int8_t b)
    {
    std::cout << "\tthunk_1b_2: b-1 is " << (char)(b-1) << "\n";
    return;
+   }
+
+static char*
+thunk_1b_3(int8_t b)
+   {
+	char result = (char)b;
+	char* ptr = &result;
+   std::cout << "\tthunk_1b_3: b is " << result << "\n";
+	return ptr;
+   }
+
+static char*
+thunk_1b_4(int8_t b)
+   {
+	char result = (char)(b-1);
+	char* ptr = &result;
+   std::cout << "\tthunk_1b_4: b-1 is " << result << "\n";
+   return ptr;
    }
 
 static void
@@ -85,7 +110,51 @@ thunk_1l_2(int64_t l)
    return;
    }
 
+static void
+thunk_1bool(uint8_t boolean)
+   {
+   std::cout << "\tthunk_1bool: boolean (1) is " << (bool)(boolean) << "\n";
+   return;
+   }
+static void
+thunk_1bool_2(uint8_t boolean)
+   {
+   std::cout << "\tthunk_1bool_2: boolean-1 (0) is " << (bool)(boolean-1) << "\n";
+   return;
+   }
+
+static void
+thunk_1c(uint16_t c)
+   {
+   std::cout << "\tthunk_1c: c (67) is " << (char)c << "\n";
+   return;
+   }
+
+static void
+thunk_1c_2(uint16_t c)
+   {
+   std::cout << "\tthunk_1c_2: c-1 (66) is " << (char)(c-1) << "\n";
+   return;
+   }
+
+static void
+thunk_1f(float f)
+   {
+	printf("Function: %G\n", f);
+   return;
+   }
+
+static void
+thunk_1f_2(float f)
+   {
+	float test = 4.5;
+   std::cout << "\tthunk_1f_2: f-1 is " << test << "\n";
+   return;
+   }
+
+
 typedef void (VoidThunkType)(void *, uintptr_t *);
+typedef void (VoidThunkTypeFloat)(void *, float *);
 
 int main(int argc, char *argv[])
    {
@@ -112,6 +181,10 @@ int main(int argc, char *argv[])
       uintptr_t args[] = { (uintptr_t) 'x' };
       f((void*)&thunk_1b, args);
       f((void*)&thunk_1b_2, args);
+      char result = f((char)&thunk_1b_3, args);
+      std::cout << result << std::endl;
+      char result = f((char)&thunk_1b_4, args);
+      std::cout << result << std::endl;
       }
 
    TR::IlType *parmTypes_1s[] = { d.toIlType<int16_t>() };
@@ -146,6 +219,45 @@ int main(int argc, char *argv[])
       f((void*)&thunk_1l, args);
       f((void*)&thunk_1l_2, args);
       }
+
+  TR::IlType *parmTypes_1c[] = { d.toIlType<uint16_t>() };
+  TR::ThunkBuilder thunk1c(&d, "1c", NoType, 1, parmTypes_1c);
+  rc = compileMethodBuilder(&thunk1c, &startPC);
+  if (rc == 0)
+	 {
+	 VoidThunkType *f = (VoidThunkType *)startPC;
+	 uintptr_t args[] = { (uintptr_t) 67 };
+	 f((void*)&thunk_1c, args);
+	 f((void*)&thunk_1c_2, args);
+	 }
+
+  TR::IlType *parmTypes_1bool[] = { d.toIlType<uint8_t>() };
+  TR::ThunkBuilder thunk1bool(&d, "1bool", NoType, 1, parmTypes_1bool);
+  rc = compileMethodBuilder(&thunk1bool, &startPC);
+  if (rc == 0)
+	{
+	VoidThunkType *f = (VoidThunkType *)startPC;
+	uintptr_t args[] = { (uintptr_t) 1 };
+	f((void*)&thunk_1bool, args);
+	f((void*)&thunk_1bool_2, args);
+	}
+
+  TR::IlType *parmTypes_1f[] = { d.toIlType<float>() };
+  TR::ThunkBuilder thunk1f(&d, "1f", NoType, 1, parmTypes_1f);
+  rc = compileMethodBuilder(&thunk1f, &startPC);
+  if (rc == 0)
+  	{
+  	VoidThunkTypeFloat *f = (VoidThunkTypeFloat *)startPC;
+  	float floatingPoint = 54.5f;
+  	uintptr_t uip2 = 0;
+  	/*uip2 = *(uintptr_t*)(uintptr_t)(float*)&floatingPoint;
+  	std::cout<< "cout: " << uip2 << std::endl;
+  	printf("Original: %G\n", floatingPoint);
+  	printf("UIP: %G\n", uip2);*/
+  	float args[] = { floatingPoint };
+  	f((void*)&thunk_1f, (float*) args);
+  	//f((void*)&thunk_1f_2, (uintptr_t*) args);
+  	}
 
    std::cout << "Step 4: shutdown JIT\n";
    shutdownJit();
